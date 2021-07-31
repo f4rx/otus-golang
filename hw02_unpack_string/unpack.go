212ccb9010b7a63956e38f83c92df61fab782618
Aleksey Stepanenko <stepanenko@selectel.ru>
2021-07-31 18:32:46 +0300

    hw02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,62 @@ package hw02unpackstring
 
 import (
 	"errors"
+	// "fmt"
+	"strconv"
+	"strings"
+	"unicode"
+
+	logger "github.com/f4rx/logger-zap-wrapper"
+	"go.uber.org/zap"
 )
+
+var slog *zap.SugaredLogger //nolint:gochecknoglobals
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func init() {
+	slog = logger.NewSugaredLogger()
+	slog.Sync() //nolint:errcheck
+}
+
+func Unpack(str string) (string, error) {
+	runes := []rune(str)
+	if len(runes) == 0 {
+		return "", nil
+	}
+
+	var outStr strings.Builder
+
+	leftRune := runes[0]
+	if unicode.IsDigit(leftRune) {
+		return "", ErrInvalidString
+	}
+	runesLen := len(runes)
+	for i := 0; i < runesLen; i++ {
+		r := runes[i]
+		slog.Debug(i, " ", string(r))
+		if i < runesLen-1 {
+			rightRune := runes[i+1]
+			if unicode.IsDigit(r) {
+				slog.Debug(r, rightRune)
+				return "", ErrInvalidString
+			}
+			if unicode.IsDigit(rightRune) {
+				c, err := strconv.Atoi(string(rightRune))
+				if err != nil {
+					slog.Panic("что-то неправильно написано....")
+				}
+				outStr.WriteString(strings.Repeat(string(r), c))
+				i++
+			} else {
+				outStr.WriteString(string(r))
+			}
+		} else {
+			if !unicode.IsDigit(r) {
+				outStr.WriteString(string(r))
+			}
+		}
+	}
+
+	return outStr.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -5,7 +5,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	// logger "github.com/f4rx/logger-zap-wrapper"
+
 )
+
+
+
 
 func TestUnpack(t *testing.T) {
 	tests := []struct {
@@ -16,6 +21,33 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "a1b2", expected: "abb"},
+		// uncomment if task with asterisk completed
+		// {input: `qwe\4\5`, expected: `qwe45`},
+		// {input: `qwe\45`, expected: `qwe44444`},
+		// {input: `qwe\\5`, expected: `qwe\\\\\`},
+		// {input: `qwe\\\3`, expected: `qwe\3`},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestUnpack1(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "a4bc2d5e", expected: "aaaabccddddde"},
+		// {input: "abccd", expected: "abccd"},
+		// {input: "", expected: ""},
+		// {input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -35,6 +67,17 @@ func TestUnpack(t *testing.T) {
 
 func TestUnpackInvalidString(t *testing.T) {
 	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	for _, tc := range invalidStrings {
+		tc := tc
+		t.Run(tc, func(t *testing.T) {
+			_, err := Unpack(tc)
+			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+
+func TestUnpackInvalidString1(t *testing.T) {
+	invalidStrings := []string{"aaa10b"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
