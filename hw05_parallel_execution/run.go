372ea24547385090fb6c89a723e986e0ec998be9
Aleksey Stepanenko <stepanenko@selectel.ru>
2021-08-02 18:49:45 +0300

    hw05_parallel_execution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -11,5 +13,30 @@ type Task func() error
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	// Place your code here.
+
+	maxGoroutines := make(chan struct{}, n)
+
+	var wg sync.WaitGroup
+	var errorCount int64 = 0
+
+	for _, task := range tasks {
+		wg.Add(1)
+		go func(task Task, errorCount *int64, m int) {
+			defer wg.Done()
+			maxGoroutines <- struct{}{}
+			if int64(m) > atomic.LoadInt64(errorCount) {
+				err := task()
+				if err != nil {
+					atomic.AddInt64(errorCount, 1)
+				}
+			}
+			<-maxGoroutines
+		}(task, &errorCount, m)
+	}
+
+	wg.Wait()
+	if errorCount >= int64(n) {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -68,3 +68,30 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
 }
+
+func TestRun1(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 23
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
+	})
+}
