e5f6160c1856fb5f12c4198076302d3686325cb2
Aleksey Stepanenko <stepanenko@selectel.ru>
2021-09-01 16:24:01 +0300

    hw01

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -6,10 +6,9 @@ import (
 	"golang.org/x/example/stringutil"
 )
 
-func main() {
-	// Place your code here.
-	strOriginal := "Hello, OTUS!"
-	strReversed := stringutil.Reverse(strOriginal)
+const strOriginal = "Hello, OTUS!"
 
+func main() {
+	strReversed := stringutil.Reverse(strOriginal)
 	fmt.Println(strReversed)
 }
