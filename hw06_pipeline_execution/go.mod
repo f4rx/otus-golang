95d5a4a4b8dbf2225b8e76ea085eac83330235ea
Aleksey Stepanenko <f1ex@inbox.ru>
2021-11-01 00:13:05 +0300

    Hw06 pipeline execution (#6)
    * hw06
    
    * hw06
    
    * hw06
    
    * hw06
    
    * hw06
    
    * fix go get
    
    * fix go get
    
    * hw06
    
    * hw06
    
    * hw06
    
    * hw06
    
    * hw06
    
    * hw06
    
    * hw06
    
    * hw06
    
    * hw06
    
    Co-authored-by: Aleksey Stepanenko <stepanenko@selectel.ru>

--- hw06_pipeline_execution/go.mod
+++ hw06_pipeline_execution/go.mod
@@ -1,10 +1,10 @@
-module github.com/fixme_my_friend/hw06_pipeline_execution
+module github.com/f4rx/otus-golang/hw06_pipeline_execution
 
 go 1.16
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/f4rx/logger-zap-wrapper v0.3.5
 	github.com/stretchr/testify v1.7.0
+	go.uber.org/zap v1.19.1
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,18 @@
 package hw06pipelineexecution
 
+import (
+	"sync"
+
+	logger "github.com/f4rx/logger-zap-wrapper"
+	"go.uber.org/zap"
+)
+
+var slog *zap.SugaredLogger //nolint:gochecknoglobals
+
+func init() {
+	slog = logger.NewSugaredLogger()
+}
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -8,7 +21,74 @@ type (
 
 type Stage func(in In) (out Out)
 
+func execStages(data interface{}, stages ...Stage) interface{} {
+	fIn := make(Bi)
+	defer close(fIn)
+	var fOut Out = fIn
+
+	if len(stages) == 0 {
+		return data
+	}
+
+	for _, stage := range stages {
+		fOut = stage(fOut)
+	}
+	fIn <- data
+	return <-fOut
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	mapMutex := &sync.Mutex{}
+	doneMutex := &sync.Mutex{}
+
+	out := make(Bi)
+	closed := false
+
+	go func() {
+		<-done
+		slog.Debug("донон")
+		doneMutex.Lock()
+		slog.Debug("лок донона")
+		if !closed {
+			slog.Debug("закрытие")
+			close(out)
+			closed = true
+		}
+		doneMutex.Unlock()
+	}()
+
+	go func() {
+		values := make(map[int]interface{})
+		var wg sync.WaitGroup
+		i := -1
+		for data := range in {
+			wg.Add(1)
+			i++
+			slog.Debug("Data: ", data)
+			go func(values map[int]interface{}, index int, data interface{}, stages ...Stage) {
+				defer wg.Done()
+				value := execStages(data, stages...)
+				mapMutex.Lock()
+				values[index] = value
+				mapMutex.Unlock()
+			}(values, i, data, stages...)
+		}
+		wg.Wait()
+
+		// Эта часть нужна чтобы оформить вывод в том же порядке в каком принимали данные
+		doneMutex.Lock()
+		slog.Debug("закрытие в основном потоке: ", closed)
+		if !closed {
+			slog.Debug("формируем вывод")
+
+			for j := 0; j <= i; j++ {
+				v := values[j]
+				out <- v
+			}
+			close(out)
+			closed = true
+		}
+		doneMutex.Unlock()
+	}()
+	return out
 }
